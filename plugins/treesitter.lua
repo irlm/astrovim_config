bac805d2715cdcfc99c3d246c3909d94d4af7182
Igor L Marques <igor.marques@gmail.com>
2023-05-13 17:08:49 -0400

    Add languages
    Signed-off-by: Igor L Marques <igor.marques@gmail.com>

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -3,7 +3,19 @@ return {
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      -- "lua"
+      "lua"
+      , "arduino"
+      , "astro"
+      , "bash"
+      , "c"
+      , "c_sharp"
+      , "cmake"
+      , "cpp"
+      , "css"
+      , "cuda"
+      , "dockerfile"
+      , "go"
+      , "rust"
     })
   end,
 }
